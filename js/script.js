c6ae4ba3462e6c9cd4c29bc8cd34243a4aade444
Juan Farias <juanfariastk@gmail.com>
2022-10-12 00:59:37 -0300

    envio com alterações no script, html e ajustes responsivos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,61 @@
+//função para consumir API com um array
+const cripto_moedas = async (lista = []) => {
+  const lista_reposta = []
+  for await (let id_moeda of lista) {
+    const url_btc = `https://www.mercadobitcoin.net/api/${id_moeda}/ticker/`
+
+    let resposta = await fetch(url_btc)
+    let resposta2 = await resposta.json()
+    lista_reposta.push(resposta2.ticker)
+  }
+
+  return lista_reposta.map(item => item.last)
+
+}
+
+//função para consumir uma API com uma string
+const cripto_moeda = async (id_moeda) => {
+
+  const url_btc = `https://www.mercadobitcoin.net/api/${id_moeda}/ticker/`
+
+  let resposta = await fetch(url_btc)
+  let resposta2 = await resposta.json()
+  return resposta2.ticker
+
+}
+
+
+//variaveis para grafico
+let array_compra = []
+let moedas = []
+let quantidades_compradas = []
+
+const div_error = document.querySelector('#erro_fim')
+const div_graf_compra = document.querySelector("#grafico_compra")
+const div_compra_graf_1 = document.querySelector("#compra_graf_1")
+
+const div_prob_graf = document.querySelector("#grafico_crescimento")
+const div_tot_graf = document.querySelector("#valor_graf")
+const div_graf_prob_fim = document.querySelector("#prob_graf")
+const div_footer_fim = document.querySelector("footer")
+
+const parag_total = document.querySelector("#total_gasto")
+
+
+//alterando paragrafo
+
+//erros possiveis 
+const erro_form = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>Selecione os campos corretamente!</strong>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+`
+const erro_form_cheio = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Já escolheu o máximo para o seu Plano!</strong>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+`
+
 //criando o 2 gráfico trading view
 
 new TradingView.widget(
@@ -50,6 +108,153 @@ investimento_mes.textContent=`R$ ${investimento_mes_valor}`
 investimento_anual.textContent=`R$ ${investimento_anual_valor}`
 propostas_disponivel.textContent=`${propostas_numeros}`
 paragrafo_mercado.textContent=`${paragrafo_text}`
+
+
+//graficos charts.js
+
+//função que recolhe os valores de um form
+function compra_moeda() {
+  const moeda_comprada = document.querySelector('#moedas_compra')
+  const quantidade_comprada = document.querySelector('#quantidade_compra')
+  let objeto_fim = { moeda: moeda_comprada.value, quantidade: quantidade_comprada.value }
+
+  if (moeda_comprada.value === 'Escolha uma moeda para comprar' || quantidade_comprada.value === 'Escolha uma quantidade') {
+    div_error.innerHTML = erro_form
+    return
+  }
+
+  return objeto_fim
+
+}
+
+//função que gera um gráfico em qualquer div, porém possui parametros especificos ao gerar
+function criar_grafico(array_desc, array_dados, array_quantidade, id_canva) {
+  new Chart(document.querySelector(`#${id_canva}`), {
+    type: 'doughnut',
+    data: {
+      labels: array_desc,
+      datasets: [{
+        label: array_desc,
+        data: [parseFloat(array_dados[0] * array_quantidade[0]).toFixed(3), parseFloat(array_dados[1] * array_quantidade[1]).toFixed(3), parseFloat(array_dados[2] * array_quantidade[2]).toFixed(3)],
+        backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+        hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+      }],
+        borderWidth: 1
+      }
+    ,
+    options: {
+      maintainAspectRatio: false,
+      tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        caretPadding: 10,
+      },
+      legend: {
+        display: false
+      },
+      cutoutPercentage: 80,
+    },
+  })
+}
+
+//função que gera um gráfico na div especificada
+function gerar_grafico_compra(array) {
+  var grafico_compra_1 = new Chart(div_graf_compra, {
+    type: 'line',
+    data: {
+      labels: [array[0].moeda, array[1].moeda, array[2].moeda],
+      datasets: [{
+        label: 'Quantidade Comprada',
+        data: [array[0].quantidade, array[1].quantidade, array[2].quantidade],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      //cutoutPercentage: 30,
+      responsive: true,
+
+    }
+  });
+}
+
+////função que gera um gráfico na div especificada
+function gerar_grafico_crescimento(array_label, array_valor) {
+  var grafico_compra_1 = new Chart(div_prob_graf, {
+    type: 'line',
+    data: {
+      labels: [array_label[0].moeda, array_label[1].moeda, array_label[2].moeda],
+      datasets: [{
+        label: 'Crescimento previsto',
+        data: [array_valor[0], array_valor[1], array_valor[2]],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      //cutoutPercentage: 30,
+      responsive: true,
+
+    }
+  });
+}
+
+//ações para gerar o gráfico
+
+
+function efetua_compra(event) {
+
+  event.preventDefault()
+
+  let compra_atual = compra_moeda()
+
+  if (array_compra.length < 3) {
+    array_compra.push(compra_atual)
+    moedas.push(compra_atual.moeda)
+    quantidades_compradas.push(compra_atual.quantidade)
+  } else {
+    cripto_moedas(moedas).then(dados => {
+      criar_grafico(moedas, dados, quantidades_compradas, 'grafico_valor_gasto')
+      let media_final= parseFloat(((parseFloat(dados[0] * parseInt(quantidades_compradas[0])).toFixed(3)) + (parseFloat(dados[1] * parseInt(quantidades_compradas[1])).toFixed(3)) + (parseFloat(dados[2] * parseInt(quantidades_compradas[2])).toFixed(3))/3)).toFixed(3)
+      parag_total.textContent = ' Média de gastos investidos: ' + media_final
+    })
+    cripto_moedas(moedas).then(dados =>{
+      gerar_grafico_crescimento(array_compra, dados)
+    })
+    gerar_grafico_compra(array_compra)
+    div_error.innerHTML = erro_form_cheio
+    div_compra_graf_1.classList.remove("d-none")
+    div_graf_prob_fim.classList.remove("d-none")
+    div_tot_graf.classList.remove("d-none")
+    div_footer_fim.classList.remove("d-none")
+  }
+
+}
+
 
 
   
